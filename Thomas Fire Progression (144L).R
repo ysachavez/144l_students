a44c8341ac5e422ba913a6d7dbd2195630f6114c
ysachavez <ychavez@ucsb.edu>
2020-10-23 11:53:53 -0700

    Thomas Fire Progression

---
+++ Thomas Fire Progression (144L).R
@@ -0,0 +1,8 @@
+# install.packages("tidyverse")
+library(tidyverse)
+# install.packages("readxl")
+library(readxl)
+
+excel_sheets("Input_Data/week1/Thomas_Fire_Progression.xlsx")
+view(thomasfire.metadata)
+
